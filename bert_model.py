7a14e8005e92140ddfe8041f97df9168b6ca9d2a
Vijay Ramakrishnan Thimmaiyah <vijayrk@cisco.com>
2020-05-03 00:58:15 -0700

    checkpoint

--- bert_model.py
+++ bert_model.py
@@ -7,13 +7,13 @@ import utils
 import torch.nn as nn
 import torch
 from transformers import BertModel, BertTokenizer
-import pandas as pd
-import itertools
-from sklearn.model_selection import train_test_split
 import numpy as np
 import torch.utils.data
 from utils import progress_bar
 from tqdm import tqdm
+from torch.utils.data import DataLoader, RandomSampler, SequentialSampler, TensorDataset
+
+import random
 
 utils.fix_random_seeds()
 SEMEVAL_HOME = os.path.join("semeval", "task9_train_pair")
@@ -24,37 +24,39 @@ logger.level = logging.ERROR
 
 
 class HfBertClassifierModel(nn.Module):
-    def __init__(self, n_classes, max_sentence_length=118, weights_name='bert-base-uncased'):
+    def __init__(self, max_sentence_length=118, weights_name='bert-base-uncased'):
         super().__init__()
-        self.n_classes = n_classes
         self.weights_name = weights_name
         self.bert = BertModel.from_pretrained(self.weights_name)
         self.hidden_dim = self.bert.embeddings.word_embeddings.embedding_dim
         self.max_sentence_length = max_sentence_length
 
         # dim : max length x max length
-        self.head = nn.Sequential(nn.Linear(self.hidden_dim, self.max_sentence_length), nn.ReLU(), nn.Linear(self.max_sentence_length, self.hidden_dim))
-        self.tail = nn.Sequential(nn.Linear(self.hidden_dim, self.max_sentence_length), nn.ReLU(), nn.Linear(self.max_sentence_length, self.hidden_dim))
-        self.W = nn.Linear(self.hidden_dim, self.n_classes)
+        self.head = nn.Sequential(
+            nn.Linear(self.hidden_dim, self.max_sentence_length),
+            nn.ReLU(),
+            nn.Linear(self.max_sentence_length, self.hidden_dim))
+
+        self.tail = nn.Sequential(
+            nn.Linear(self.hidden_dim, self.max_sentence_length),
+            nn.ReLU(),
+            nn.Linear(self.max_sentence_length, self.hidden_dim))
+
         # initialize a random tensor
-        #self.labels = 2
         self.L = torch.randn(self.hidden_dim, self.hidden_dim)
 
     def bilinear(self, head, tail):
-        # first, each head/tail is batchsize x n x n, so we need to reshape into bn x d
-
-        # Do the multiplications
-        # (bn x d) (d x d) -> (bn x d)
         lin = torch.mm(head.reshape([-1, self.hidden_dim]), self.L).reshape([-1, self.max_sentence_length, self.hidden_dim])
         bi_lin = torch.matmul(lin, tail.reshape([-1, self.hidden_dim, self.max_sentence_length]))
-        # (bn x d) (bn x d)T -> (bn x bn)
-        #tail = tail.reshape(int(tail.shape[0]*tail.shape[1]*tail.shape[2]/self.hidden_dim), self.hidden_dim)
-        # lin = torch.mm(lin, tail)
-        #import pdb; pdb.set_trace()
-        #import pdb; pdb.set_trace()
         return torch.sigmoid(bi_lin)
 
-    def forward(self, X):
+    def forward(self,
+                input_ids=None,
+                attention_mask=None,
+                token_type_ids=None,
+                position_ids=None,
+                head_mask=None,
+                inputs_embeds=None,):
         """Here, `X` is an np.array in which each element is a pair
         consisting of an index into the BERT embedding and a 1 or 0
         indicating whether the token is masked. The `fit` method will
@@ -62,21 +64,19 @@ class HfBertClassifierModel(nn.Module):
 
         The shape of X is batchsize x 2 x max_sentence_length
         """
-        indices = X[:, 0, :]
-        # Type conversion, since the base class insists on
-        # casting this as a FloatTensor, but we ned Long
-        # for `bert`.
-        indices = indices.long()
-        mask = X[:, 1, :]
         (final_hidden_states, cls_output) = self.bert(
-            indices, attention_mask=mask)
+            input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+        )
         head = self.head(final_hidden_states)
         tail = self.tail(final_hidden_states)
         # for the forward pass, we need to return a score and the tensor
         output = self.bilinear(head, tail)
         return output
-        # import pdb; pdb.set_trace()
-        # return self.W(cls_output)
 
 
 class HfBertClassifier(TorchShallowNeuralClassifier):
@@ -89,7 +89,7 @@ class HfBertClassifier(TorchShallowNeuralClassifier):
         super().__init__(*args, **kwargs)
 
 
-    def fit(self, X, y, **kwargs):
+    def fit(self, X, **kwargs):
         """Standard `fit` method.
 
         Parameters
@@ -111,19 +111,15 @@ class HfBertClassifier(TorchShallowNeuralClassifier):
         if X_dev is not None:
             dev_iter = kwargs.get('dev_iter', 10)
         # Data prep:
-        X = np.array(X)
-        self.input_dim = X.shape[1]
-        self.classes_ = sorted(set(y))
-        self.n_classes_ = len(self.classes_)
-        class2index = dict(zip(self.classes_, range(self.n_classes_)))
-        y = [class2index[label] for label in y]
-        # Dataset:
-        X = torch.FloatTensor(X)
-        y = torch.tensor(y)
-        dataset = torch.utils.data.TensorDataset(X, y)
+        all_input_ids = torch.tensor([f.input_ids for f in X], dtype=torch.long)
+        all_input_mask = torch.tensor([f.input_mask for f in X], dtype=torch.long)
+        all_segment_ids = torch.tensor([f.segment_ids for f in X], dtype=torch.long)
+        all_rels = torch.tensor([f.rels for f in X], dtype=torch.float)
+        dataset = TensorDataset(all_input_ids, all_input_mask, all_segment_ids, all_rels)
         dataloader = torch.utils.data.DataLoader(
             dataset, batch_size=self.batch_size, shuffle=True,
             pin_memory=True)
+
         # Graph:
         if not self.warm_start or not hasattr(self, "model"):
             self.model = self.define_graph()
@@ -131,28 +127,36 @@ class HfBertClassifier(TorchShallowNeuralClassifier):
                 self.model.parameters(),
                 lr=self.eta,
                 weight_decay=self.l2_strength)
+
         self.model.to(self.device)
         self.model.train()
         # Optimization:
-        loss = nn.CrossEntropyLoss()
+        loss = nn.BCELoss()
         # Train:
         with tqdm(total=self.max_iter) as pbar:
             for iteration in range(1, self.max_iter+1):
                 epoch_error = 0.0
-                for X_batch, y_batch in dataloader:
-                    X_batch = X_batch.to(self.device, non_blocking=True)
-                    y_batch = y_batch.to(self.device, non_blocking=True)
-                    batch_preds = self.model(X_batch)
-                    err = loss(batch_preds, y_batch)
+                for batch in dataloader:
+                    inputs = {"input_ids": batch[0], "attention_mask": batch[1]}
+                    inputs["token_type_ids"] = (batch[2])
+                    batch_preds = self.model(**inputs)
+                    err = loss(batch_preds.reshape([-1, self.max_sentence_length]),
+                               batch[3].reshape([-1, self.max_sentence_length]))
                     epoch_error += err.item()
                     self.opt.zero_grad()
                     err.backward()
                     self.opt.step()
+
                 # Incremental predictions where possible:
                 if X_dev is not None and iteration > 0 and iteration % dev_iter == 0:
                     self.dev_predictions[iteration] = self.predict(X_dev)
                     self.model.train()
+
                 self.errors.append(epoch_error)
+
+                print("Finished epoch {} of {}; error is {}".format(
+                        iteration, self.max_iter, epoch_error))
+
                 progress_bar(
                     "Finished epoch {} of {}; error is {}".format(
                         iteration, self.max_iter, epoch_error))
@@ -165,10 +169,47 @@ class HfBertClassifier(TorchShallowNeuralClassifier):
         new BERT-based graph.
 
         """
-        bert = HfBertClassifierModel(
-            self.n_classes_, weights_name=self.weights_name, max_sentence_length=self.max_sentence_length)
+        bert = HfBertClassifierModel(weights_name=self.weights_name, max_sentence_length=self.max_sentence_length)
         bert.train()
         return bert
+
+    def predict(self, X):
+        """Predicted probabilities for the examples in `X`.
+
+        Parameters
+        ----------
+        X : np.array
+
+        Returns
+        -------
+        np.array with shape (len(X), self.n_classes_)
+
+        """
+        self.model.eval()
+        with torch.no_grad():
+            self.model.to(self.device)
+            # Data prep:
+            all_input_ids = torch.tensor([f.input_ids for f in X], dtype=torch.long).to(self.device)
+            all_input_mask = torch.tensor([f.input_mask for f in X], dtype=torch.long).to(self.device)
+            all_segment_ids = torch.tensor([f.segment_ids for f in X], dtype=torch.long).to(self.device)
+            all_rels = torch.tensor([f.rels for f in X], dtype=torch.float).to(self.device)
+            dataset = TensorDataset(all_input_ids, all_input_mask, all_segment_ids, all_rels)
+            dataloader = torch.utils.data.DataLoader(
+                dataset, batch_size=self.batch_size, shuffle=True,
+                pin_memory=True)
+
+            preds = []
+            for batch in dataloader:
+                inputs = {"input_ids": batch[0], "attention_mask": batch[1]}
+                inputs["token_type_ids"] = (batch[2])
+                batch_preds = self.model(**inputs)
+                preds.append(torch.round(batch_preds).cpu().numpy())
+
+            result = preds[0]
+            for i in range(len(preds) - 1):
+                np.vstack((result, preds[i + 1]))
+            return result
+
 
     def encode(self, X, max_length=None):
         """The `X` is a list of strings. We use the model's tokenizer
@@ -193,69 +234,6 @@ class HfBertClassifier(TorchShallowNeuralClassifier):
 
 DB_dataset = data_processor.Dataset('DrugBank').from_training_data('DrugBank')
 ML_dataset = data_processor.Dataset('MedLine').from_training_data('MedLine')
-
-
-def create_classification_task(dataset, maxsize=None):
-    '''Take a dataprocessor dataset object and return a Pandas dataframe for classification task '''
-
-    classification_task_df = pd.DataFrame(columns=['e1_id','e1_type','e1_name','e2_id','e2_type','e2_name','sentence','ddi','label'])
-    count = 0
-    for doc in dataset.documents:
-        for sent in doc.sentences:
-            if len(sent.map)==0:
-                if len(sent.entities)==1:
-                    classification_task_df = classification_task_df.append({
-                        'e1_id': sent.entities[0]._id,
-                        'e1_type': sent.entities[0].type,
-                        'e1_name': sent.entities[0].text,
-                        'e2_id':"",
-                        'e2_type':"",
-                        'e2_name':"",
-                        'sentence':sent.text,
-                        'ddi':False,
-                        'label':'NO_DDI'
-                    },ignore_index=True)
-                elif len(sent.entities)>2:
-                    for (i,j)in itertools.combinations(range(len(sent.entities)),2):
-                        classification_task_df = classification_task_df.append({
-                            'e1_id': sent.entities[i]._id,
-                            'e1_type': sent.entities[i].type,
-                            'e1_name': sent.entities[i].text,
-                            'e2_id':sent.entities[j]._id,
-                            'e2_type':sent.entities[j].type,
-                            'e2_name':sent.entities[j].text,
-                            'sentence':sent.text,
-                            'ddi':False,
-                            'label':'NO_DDI'
-                        },ignore_index=True)
-            elif len(sent.map) > 0: # if there is a pair
-                for i, (k, v) in enumerate(sent.map.items()):
-                    for entity in sent.entities:
-                        if entity._id == k:
-                            e1_id = k
-                            e1_type = entity.type
-                            e1_name = entity.text
-                        if entity._id == v[0]:
-                            e2_id = v[0]
-                            e2_type = entity.type
-                            e2_name = entity.text
-                    classification_task_df = classification_task_df.append({
-                            'e1_id': e1_id,
-                            'e1_type':e1_type,
-                            'e1_name':e1_name,
-                            'e2_id':e2_id,
-                            'e2_type':e2_type,
-                            'e2_name':e2_name,
-                            'sentence':sent.text,
-                            'ddi':True,
-                            'label':v[1]
-                        },ignore_index=True)
-            count += 1
-            if maxsize and count > maxsize:
-                return classification_task_df
-    return classification_task_df
-
-
 
 class InputFeatures(object):
     """A single set of features of data."""
@@ -291,9 +269,19 @@ def convert_examples_to_features(
     """
     features = []
 
+    counter = 0
+
     for doc in dataset.documents:
         for sent in doc.sentences:
-            print(sent.text)
+
+            if counter > 100:
+                break
+
+            counter += 1
+
+            if sent.text == '':
+                continue
+
             word_tokens = sent.text.split()
             relation_pairs = [[] for _ in range(len(word_tokens))]
 
@@ -304,9 +292,14 @@ def convert_examples_to_features(
 
             entity_map = {}
             for entity in sent.entities:
-                entity_map[entity._id] = entity.char_offset
+                if len(entity.char_offset.split()) == 2:
+                    entity_map[entity._id] = entity.char_offset
 
             for key in sent.map:
+
+                if key not in entity_map:
+                    continue
+
                 source_start, source_end = [int(x) for x in entity_map[key].split('-')]
                 dst_start, dst_end = [int(x) for x in entity_map[sent.map[key][0]].split('-')]
 
@@ -345,20 +338,17 @@ def convert_examples_to_features(
             relation_pairs_tokenized = []
 
             tokens = [cls_token]
-
             for i, word_i in enumerate(word_tokens):
                 src_word_tokens = tokenizer.tokenize(word_i)
                 for tok in src_word_tokens:
                     relation_pairs_tokenized.append(relation_pairs[i])
                     tokens.append(tok)
 
+            if len(tokens) > max_seq_length - 2:
+                continue
+
             relation_pairs_tokenized = np.asarray(relation_pairs_tokenized)
-
-            # if sent.text == 'Table 1 Changes in Desloratadine and 3-Hydroxydesloratadine Pharmacokinetics in Healthy Male and Female Volunteers':
-            #     #import pdb; pdb.set_trace()
-            #     pass
-
-            relation_pairs_tokenized_2 = np.asarray(relation_pairs_tokenized)[:,0]
+            relation_pairs_tokenized_2 = relation_pairs_tokenized[:,0]
 
             for j, word_j in enumerate(word_tokens):
                 dst_word_tokens = tokenizer.tokenize(word_j)
@@ -425,11 +415,16 @@ def run_experiment(batch_size=16, max_iter=4, eta=0.00002, test_size=0.2, random
     print('Running exp')
     max_sentence_length = 120
     tokenizer = BertTokenizer.from_pretrained('bert-base-cased')
-    convert_examples_to_features(DB_dataset, max_sentence_length, tokenizer)
-    classification_task_df = pd.concat([
-        create_classification_task(DB_dataset, maxsize=datasize),
-        create_classification_task(ML_dataset, maxsize=datasize)
-    ])
+    examples = convert_examples_to_features(DB_dataset, max_sentence_length, tokenizer)
+    random.seed(42)
+    random.shuffle(examples)
+
+    train_index = int(0.7 * len(examples))
+    dev_index = int(0.8 * len(examples))
+
+    train = examples[:train_index]
+    dev = examples[train_index:dev_index]
+    test = examples[dev_index:]
 
     bert_experiment_1 = HfBertClassifier(
         'bert-base-uncased',
@@ -438,14 +433,14 @@ def run_experiment(batch_size=16, max_iter=4, eta=0.00002, test_size=0.2, random
         max_iter=max_iter,
         eta=eta)
 
-    X_text_train, X_text_test, y_train, y_test = train_test_split(classification_task_df['sentence'], classification_task_df['label'], test_size=test_size, random_state=random_state)
-    import pdb; pdb.set_trace()
-    X_indices_train = bert_experiment_1.encode(X_text_train, max_length=max_sentence_length)
-    X_indices_test = bert_experiment_1.encode(X_text_test, max_length=max_sentence_length)
-    time = bert_experiment_1.fit(X_indices_train, y_train)
-    bert_experiment_1_preds = bert_experiment_1.predict(X_indices_test)
-    print(classification_report(bert_experiment_1_preds, y_test , digits=3))
-    bert_experiment_1.to_pickle('BERT_exp1.pkl')
+    time = bert_experiment_1.fit(train, **{'X_dev': dev})
+    bert_experiment_1_preds = bert_experiment_1.predict(test)
+
+    print(classification_report(bert_experiment_1_preds.reshape(-1),
+                                np.asarray([item.rels for item in test]).reshape(-1),
+                                digits=3))
+
+    #bert_experiment_1.to_pickle('BERT_exp1.pkl')
 
 
 run_experiment(datasize=200)
